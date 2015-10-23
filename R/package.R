48bcc4e33f4d760235e2936b762373757f1d712b
Gabor Csardi <csardi.gabor@gmail.com>
2015-10-23 17:21:01 +0100

    dev

--- R/package.R
+++ R/package.R
@@ -4,9 +4,22 @@
 #' @param file Name of the image file to show.
 #'
 #' @importFrom png readPNG
+#' @importFrom tools file_ext
 #' @export
 
 show_image <- function(file) {
 
+  if (!file.exists(file)) stop("File does not exist: ", file)
+  
+  ext <- tolower(file_ext(file))
+
+  if (ext == "png") {
+    show_image_png(file)
+  } else {
+    stop("Unknown image type")
+  }
 }
 
+show_image_png <- function(file) {
+  
+}
