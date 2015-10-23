b23e58938b2ca5fc6e08137c8286931e8e850361
Gabor Csardi <csardi.gabor@gmail.com>
2015-10-23 09:50:08 +0100

    Initial commit of Mason template

---
+++ R/package.R
@@ -0,0 +1,8 @@
+
+#' Show an Image on an R Graphics Device
+#'
+#' sometimes it is handy to be able to view an image file on an R graphics device.
+#'
+#' @docType package
+#' @name showimage
+NULL
---
+++ tests/testthat/test.R
@@ -0,0 +1,8 @@
+
+context("showimage")
+
+test_that("showimage works", {
+
+  expect_true(TRUE)
+
+})
